6e14b524f6d04edb35b0cd416d08a02330d51d37
Emmanuella <emmavduru@gmail.com>
2021-01-28 20:32:35 +0100

    Set up util file with the validation functions

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -1,3 +1,9 @@
+const {
+  validateRuleandData,
+  ruleFieldValidate,
+  validateRule,
+} = require("../utils");
+
 const response = {
   message: "",
   status: "",
@@ -24,56 +30,11 @@ const validate_rule_post = (req, res) => {
   const { rule, data } = req.body;
 
   // Check if the rule and data properties are valid
-  const validateRuleandData = (rule, data) => {
-    if (!rule || !data) {
-      response.message = !rule ? "rule is required." : "data is required.";
-      return false;
-    } else if (typeof rule !== "object") {
-      response.message = "rule should be an object";
-      return false;
-    } else if (typeof data !== "object" && typeof data !== "string") {
-      response.message = "data should be an object, array or string.";
-      return false;
-    } else {
-      return true;
-    }
-  };
 
-  // Validates the properties in the rule field
-  const validateRule = (rule, data) => {
-    if (!rule.field || !rule.condition || !rule.condition_value) {
-      response.message = !rule.field
-        ? "rule field is required."
-        : !rule.condition
-        ? "rule condition is required."
-        : "rule condition value is required.";
-      return false;
-    } else if (!data[rule.field]) {
-      response.message = `field ${rule.field} is missing from data.`;
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  // Check if the rule field condition is true
-  const ruleFieldValidate = (rule, data) => {
-    const result =
-      rule.condition === "eq"
-        ? data[rule.field] === rule.condition_value
-        : rule.condition === "neq"
-        ? data[rule.field] !== rule.condition_value
-        : rule.condition === "gt"
-        ? data[rule.field] > rule.condition_value
-        : rule.condition === "gte"
-        ? data[rule.field] >= rule.condition_value
-        : rule.condition === "contains"
-        ? data[rule.fields].includes(rule.condition_value)
-        : "";
-    return result;
-  };
-
-  if (!validateRuleandData(rule, data) || !validateRule(rule, data)) {
+  if (
+    !validateRuleandData(rule, data, response) ||
+    !validateRule(rule, data, response)
+  ) {
     response.status = "error";
     response.data = null;
     res.status(400);
